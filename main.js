7eba4cd25310f325a3e3cdcac5ab7e51b8fbfb6e
najwanan <theunnamedrd@gmail.com>
2022-08-04 18:35:08 -0700

    setting up fetch statement

---
+++ main.js
@@ -0,0 +1,14 @@
+document.querySelector("button").addEventListener("click", apiRequest);
+
+async function apiRequest() {
+  const alienName = document.querySelector("input").value;
+  try {
+    const response = await fetch(
+      `https://trekki-alien-api.herokuapp.com/api/${alienName}`
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
