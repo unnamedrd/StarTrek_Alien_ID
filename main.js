0159098199c1384d3278be23f31a05ba3859d7cc
najwanan <theunnamedrd@gmail.com>
2022-08-04 19:59:57 -0700

    adding queery selectors for form outputs

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-document.querySelector("button").addEventListener("click", apiRequest);
+document.querySelector("#button").addEventListener("click", apiRequest);
 
+//document.getElementById("getButton").addEventListener("click", test);
+
+function test() {
+    console.log('this event listener is working')
+}
 async function apiRequest() {
   const alienName = document.querySelector("input").value;
   try {
@@ -8,6 +13,15 @@ async function apiRequest() {
     );
     const data = await response.json();
     console.log(data);
+
+    document.getElementById("alienName").innerHTML = data.speciesName;
+    document.getElementById("alienWorld").innerHTML = data.homeworld;
+    document.getElementById("alienFeatures").innerHTML = data.features;
+    document.getElementById("alienFacts").innerHTML = data.interestingFact;
+    document.getElementById("alienExamples").innerHTML = data.notableExamples;
+
+    document.getElementById("alienImage").src = data.image, 
+    document.getElementById("alienCaption").innerText = data.speciesName;
   } catch (error) {
     console.log(error);
   }
